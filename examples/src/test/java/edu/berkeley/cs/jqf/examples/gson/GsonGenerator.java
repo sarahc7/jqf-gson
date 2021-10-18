d52316eaa1b5b1623532c996943b31cc0d440955
sarahc7 <sarah.cai@gmail.com>
2021-10-18 14:52:11 -0700

    Partial progress on integrating JavaPoet

--- examples/src/test/java/edu/berkeley/cs/jqf/examples/gson/GsonGenerator.java
+++ examples/src/test/java/edu/berkeley/cs/jqf/examples/gson/GsonGenerator.java
@@ -27,10 +27,10 @@ public class GsonGenerator extends Generator<Pair> {
         return generateObject(random, "Main", new ArrayList<Pair<String, String>>(), new ArrayList<Pair<String, String>>());
     }
 
-    private Pair<String, String> generateElement(SourceOfRandomness random,
-                                                 String name,
-                                                 List<Pair<String, String>> fieldValues,
-                                                 List<Pair<String, String>> arrayValues) {
+    private Pair<String, FieldSpec> generateElement(SourceOfRandomness random,
+                                                    String name,
+                                                    List<Pair<String, String>> fieldValues,
+                                                    List<Pair<String, String>> arrayValues) {
         return random.choose(Arrays.<Supplier<Pair<String, String>>>asList(
                 () -> generateObject(random, name, fieldValues, arrayValues),
                 () -> generateArray(random, name, fieldValues, arrayValues),
@@ -52,10 +52,12 @@ public class GsonGenerator extends Generator<Pair> {
         return s;
     }
 
-    private String generateConstructor(String name, List<Pair<String, String>> fieldValues, List<Pair<String, String>> arrayValues) {
-        String constructor = "public " + name + "() {\n";
+    private MethodSpec generateConstructor(String name, List<Pair<String, String>> fieldValues, List<Pair<String, String>> arrayValues) {
+        MethodSpec.Builder constructor = MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PUBLIC);
+
         for (Pair<String, String> field : fieldValues) {
-            constructor += "this." + field.getKey() + " = " + field.getValue() + ";\n";
+            constructor.addStatement("this.$N = $L", field.getKey(), field.getValue());
         }
 
         Map<String, Integer> indices = new HashMap<>();
@@ -63,55 +65,56 @@ public class GsonGenerator extends Generator<Pair> {
             String arrayName = arrayElement.getKey(), value = arrayElement.getValue();
             if (!indices.containsKey(arrayName)) indices.put(arrayName, 0);
 
-            constructor += "this." + arrayName + "[" + indices.get(arrayName) + "] = " + value + ";\n";
+            constructor.addStatement("this.$N[$L] = $L", arrayName, indices.get(arrayName), value);
+            // constructor += "this." + arrayName + "[" + indices.get(arrayName) + "] = " + value + ";\n";
             indices.put(arrayName, indices.get(arrayName) + 1);
         }
-        constructor += "}\n";
 
-        return constructor;
+        return constructor.build();
     }
 
-    private Pair<String, String> generateObject(SourceOfRandomness random,
+    private Pair<String, TypeSpec> generateObject(SourceOfRandomness random,
                                                 String name,
                                                 List<Pair<String, String>> fieldValues,
                                                 List<Pair<String, String>> arrayValues) {
         String className = generateClassName(random);
 
         List<Pair<String, String>> innerClassFieldValues = new ArrayList<Pair<String, String>>(), innerClassArrayValues = new ArrayList<Pair<String, String>>();
-        Pair<String, String> members = generateMembers(random, innerClassFieldValues, innerClassArrayValues);
+        Pair<String, List<FieldSpec>> fields = generateFields(random, innerClassFieldValues, innerClassArrayValues);
 
-        String field = "", modifier = "public";
+        TypeSpec.Builder obj = TypeSpec.classBuilder(name)
+                .addModifiers(Modifier.PUBLIC)
+                .addFields(fields);
+
+        // String field = "", modifier = "public";
         if (!name.equals("Main")) {
-            field = "public " + className + " " + name + ";\n";
+            obj.addModifiers(Modifier.STATIC);
+            obj.addField(ClassName.get("test", className), name, Modifier.PUBLIC);
+            // field = "public " + className + " " + name + ";\n";
             fieldValues.add(new Pair<String, String>(name, "new " + className + "()"));
-            modifier += " static";
+            // modifier += " static";
         }
-        return new Pair<>("{" + members.getKey() + "}", field + modifier + " class " + className + " {\n" +
-                members.getValue() + generateConstructor(className, innerClassFieldValues, innerClassArrayValues) + "}");
 
-//        if (name.equals("Main")) return object;
-//        else {
-//            String field = "public " + className + " " + name + ";\n";
-//            fieldValues.add(new Pair<String, String>(name, "new " + className + "();"));
-//            return new Pair<>(object.getKey(), field + "static " + object.getValue());
-//        }
+        obj.addMethod(generateConstructor(className, innerClassFieldValues, innerClassArrayValues));
+
+        return new Pair<>("{" + members.getKey() + "}", obj.build());
     }
 
-    private Pair<String, String> generateMembers(SourceOfRandomness random,
-                                                 List<Pair<String, String>> fieldValues,
-                                                 List<Pair<String, String>> arrayValues) {
+    private Pair<String, List<FieldSpec>> generateFields(SourceOfRandomness random,
+                                                          List<Pair<String, String>> fieldValues,
+                                                          List<Pair<String, String>> arrayValues) {
         int maxMembers = random.nextInt(4);
         String members = "";
-        String javaFields = "";
+        List<FieldSpec> fields = new ArrayList<>();
         for (int i = 0; i < maxMembers; i++) {
             if (i > 0)  members += ", ";
 
-            Pair<String, String> p = generateMember(random, fieldValues, arrayValues);
+            Pair<String, FieldSpec> p = generateField(random, fieldValues, arrayValues);
             members += p.getKey();
-            javaFields += p.getValue() + "\n";
+            fields.add(p.getValue());
         }
 
-        return new Pair<>(members, javaFields);
+        return new Pair<>(members, fields);
     }
 
     private String generateIdentifier(SourceOfRandomness random) {
@@ -120,45 +123,50 @@ public class GsonGenerator extends Generator<Pair> {
         return s;
     }
 
-    private Pair<String, String> generateMember(SourceOfRandomness random,
-                                                List<Pair<String, String>> fieldValues,
-                                                List<Pair<String, String>> arrayValues) {
+    private Pair<String, FieldSpec> generateField(SourceOfRandomness random,
+                                                   List<Pair<String, String>> fieldValues,
+                                                   List<Pair<String, String>> arrayValues) {
         String name = generateIdentifier(random);
-        Pair<String, String> value = generateElement(random, name, fieldValues, arrayValues);
+        Pair<String, FieldSpec> value = generateElement(random, name, fieldValues, arrayValues);
 
         String member = "\"" + name + "\": " + value.getKey();
 
         return new Pair<>(member, value.getValue());
     }
 
-    private Pair<String, String> generateArray(SourceOfRandomness random,
+    private Pair<String, FieldSpec> generateArray(SourceOfRandomness random,
                                                String name,
                                                List<Pair<String, String>> fieldValues,
                                                List<Pair<String, String>> arrayValues) {
-        String type = random.choose(new String[]{"int", "boolean", "String"}), elements = "";
+        String type = random.choose(new String[]{"int", "bool", "String"});
+        List<String> elements = new ArrayList<>();
         int numElements = random.nextInt(4);
-        fieldValues.add(new Pair<String, String>(name, "new " + type + "[" + numElements + "]"));
 
         for (int i = 0; i < numElements; i++) {
-            if (i > 0)  elements += ", ";
-
-            String element;
             if (type.equals("int")) {
-                element = generateInteger(random, name, new ArrayList<Pair<String, String>>()).getKey();
-            } else if (type.equals("boolean")) {
-                element = generateBool(random, name, new ArrayList<Pair<String, String>>()).getKey();
+                elements.add(generateInteger(random, name, new ArrayList<Pair<String, String>>()).getKey());
+            } else if (type.equals("bool")) {
+                elements.add(generateBool(random, name, new ArrayList<Pair<String, String>>()).getKey());
             } else {
-                element = generateString(random, name, new ArrayList<Pair<String, String>>()).getKey();
+                elements.add(generateString(random, name, new ArrayList<Pair<String, String>>()).getKey());
             }
-
-            elements += element;
-            arrayValues.add(new Pair<String, String>(name, element));
         }
 
-        return new Pair<String, String>("[" + elements + "]", "public " + type + "[] " + name + ";");
+        String arrayContents = elements.stream().collect(Collectors.joining(","));
+
+        FieldSpec.Builder arrayField;
+        if (type.equals("int")) {
+            arrayField = FieldSpec.builder(int[].class, name);
+        } else if (type.equals("bool")) {
+            arrayField = FieldSpec.builder(bool[].class, name);
+        } else {
+            arrayField = FieldSpec.builder(String[].class, name);
+        }
+        return new Pair<String, String>("[" + arrayContents + "]",
+                arrayField.addModifiers(Modifier.PUBLIC).initializer("{$L}", arrayContents).build());
     }
 
-    private Pair<String, String> generateString(SourceOfRandomness random,
+    private Pair<String, FieldSpec> generateString(SourceOfRandomness random,
                                                 String name,
                                                 List<Pair<String, String>> fieldValues) {
         int len = random.nextInt(6);
@@ -167,11 +175,14 @@ public class GsonGenerator extends Generator<Pair> {
             s += random.nextChar('a', 'z');
         s += "\"";
 
-        fieldValues.add(new Pair<String, String>(name, s));
-        return new Pair<String, String>(s, "public String " + name + ";");
+        FieldSpec stringField = FieldSpec.builder(String.class, name)
+                .addModifiers(Modifier.PUBLIC)
+                .initializer("$S", s)
+                .build();
+        return new Pair<String, String>(s, stringField);
     }
 
-    private Pair<String, String> generateInteger(SourceOfRandomness random,
+    private Pair<String, FieldSpec> generateInteger(SourceOfRandomness random,
                                                  String name,
                                                  List<Pair<String, String>> fieldValues) {
         String num = random.choose(Arrays.<Supplier<String>>asList(
@@ -180,9 +191,12 @@ public class GsonGenerator extends Generator<Pair> {
                 () -> "-" + random.nextInt(10),
                 () -> "-" + random.nextInt(1, 10) + generateDigits(random)
         )).get();
-        fieldValues.add(new Pair<String, String>(name, num));
 
-        return new Pair<>(num, "public int " + name + ";");
+        FieldSpec intField = FieldSpec.builder(int.class, name)
+                .addModifiers(Modifier.PUBLIC)
+                .initializer("$L", num)
+                .build();
+        return new Pair<>(num, intField);
     }
 
     private String generateDigits(SourceOfRandomness random) {
@@ -192,23 +206,30 @@ public class GsonGenerator extends Generator<Pair> {
         return digits;
     }
 
-    private Pair<String, String> generateBool(SourceOfRandomness random,
+    private Pair<String, FieldSpec> generateBool(SourceOfRandomness random,
                                               String name,
                                               List<Pair<String, String>> fieldValues) {
         String bool = random.choose(Arrays.<Supplier<String>>asList(
                 () -> "false",
                 () -> "true"
         )).get();
-        fieldValues.add(new Pair<String, String>(name, bool));
 
-        return new Pair<>(bool, "public boolean " + name + ";");
+        FieldSpec boolField = FieldSpec.builder(bool.class, name)
+                .addModifiers(Modifier.PUBLIC)
+                .initializer("$L", bool)
+                .build();
+        return new Pair<>(bool, nullField);
     }
 
-    private Pair<String, String> generateNull(SourceOfRandomness random,
+    private Pair<String, FieldSpec> generateNull(SourceOfRandomness random,
                                               String name,
                                               List<Pair<String, String>> fieldValues) {
-        fieldValues.add(new Pair<String, String>(name, "null"));
-        return new Pair<>("null", "public String " + name + ";");
+        FieldSpec nullField = FieldSpec.builder(String.class, name)
+                .addModifiers(Modifier.PUBLIC)
+                .initializer("null")
+                .build();
+
+        return new Pair<>("null", nullField);
     }
 
 }
